e56ce10ff5716933ca701c31e73d82cc2679c4e8
“chak12346789@gmail.com” <chak12346789@gmail.com>
2023-09-06 16:45:49 +0800

    加入專案檔案。

---
+++ LibreOpen/Program.cs
@@ -0,0 +1,34 @@
+using LibreOpen.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectString = builder.Configuration["ConnectionString:DefaultConnection"];
+
+builder.Services.AddDbContext<StudentDataContext>(optionsBuilder => optionsBuilder.UseMySQL(connectString));
+
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
